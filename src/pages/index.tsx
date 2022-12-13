5a536c854466cc6763695efbe6c015ee4c606d9f
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2022-12-13 21:34:28 +0200

    Hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import Navigation from 'src/components/Navigation';
+import HeroSection from 'src/components/HeroSection';
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Start Up Web</h1>;
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <HeroSection />
+
+      <div className="container mx-auto text-center py-8">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem netus
+          lacinia convallis nisl, <br /> metus. Cras dui, amet urna feugiat.
+          Risus ut adipiscing enim{' '}
+        </p>
+      </div>
+
+      <div className="min-h-screen"></div>
+    </div>
+  );
 }
