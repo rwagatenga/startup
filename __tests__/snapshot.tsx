54db413e1b78bc6b260134d9a2101cc05ced8df6
Fred_Rwagatenga <fred.rwagatenga@gitstart.dev>
2023-02-15 10:40:45 +0200

    initial commit

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Home from '../src/pages/index';
+import Home from '@/pages/index';
 
 it('renders homepage unchanged', () => {
   const { container } = render(<Home />);
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  className?: string
+};
+const Card = ({children, className}: Props) => {
+  return (
+    <div className={`md:mx-auto rounded-xl shadow-md overflow-hidden px-2 py-2 ${className}`}>
+        {children}
+    </div>
+  )
+};
+
+export default Card;
