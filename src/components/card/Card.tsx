c255f2c9e2a57d7b7bc2469c677d5430780cc188
Fred_Rwagatenga <fred.rwagatenga@gitstart.dev>
2023-02-15 10:44:27 +0200

    fix lint issues

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,14 +2,16 @@ import React, { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
-  className?: string
+  className?: string;
 };
-const Card = ({children, className}: Props) => {
+const Card = ({ children, className }: Props) => {
   return (
-    <div className={`md:mx-auto rounded-xl shadow-md overflow-hidden px-2 py-2 ${className}`}>
-        {children}
+    <div
+      className={`md:mx-auto rounded-xl shadow-md overflow-hidden px-2 py-2 ${className}`}
+    >
+      {children}
     </div>
-  )
+  );
 };
 
 export default Card;
