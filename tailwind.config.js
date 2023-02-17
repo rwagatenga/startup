b7f058ee124351545e56a266019d36a1efcc32e9
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-02-17 14:17:47 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        100: '#00D1FF',
+        200: '#00A6CB',
+        300: '#022F62',
+        400: '#09284D',
+        500: '#013647',
+        600: '#011B2D',
+        700: '#010209',
+      },
+      green: {
+        100: '#37FA2C',
+        200: '#172617',
+      },
+      yellow: '#DFF200',
+      white: colors.white,
+      gray: colors.gray,
+      transparent: 'transparent',
+      current: 'currentColor',
+    },
   },
   plugins: [],
 };
