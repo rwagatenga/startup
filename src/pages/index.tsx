27e0d12c1673ea4b5919a681cd11ec31b70df4b2
Fred_Rwagatenga <fred.rwagatenga@gitstart.dev>
2023-04-06 09:40:20 +0200

    revert file changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,4 @@
-import Navigation from 'src/components/Navigation';
-import HeroSection from 'src/components/HeroSection';
+import Card from '@/components/card/Card';
 export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <Navigation />
-      <HeroSection />
-
-      <div className="container mx-auto text-center py-8">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem netus
-          lacinia convallis nisl, <br /> metus. Cras dui, amet urna feugiat.
-          Risus ut adipiscing enim{' '}
-        </p>
-      </div>
-
-      <div className="min-h-screen"></div>
-    </div>
-  );
+  return <Card>Hello</Card>;
 }
